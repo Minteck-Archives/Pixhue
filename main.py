39473f4a249db4595059f3a4614c5c8c1f0fa162
Minteck <nekostarfan@gmail.com>
2021-05-18 22:00:23 +0200

    Stack Management

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, logging, json
 from PIL import Image
 
 # ----------------
@@ -6,6 +6,10 @@ from PIL import Image
 debug = False
 
 # ----------------
+
+logging.basicConfig(filename='./yagpl.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger=logging.getLogger(__name__)
 
 try:
     sys.argv[1]
@@ -26,6 +30,8 @@ pyMode = False
 pyCode = ""
 index = 0
 row = 0
+global programStack
+programStack = []
 
 if debug:
     print("Parsing pixels...")
@@ -34,52 +40,84 @@ for x in range(width):
     column = 0
 
     for y in range(height):
-        column = column + 1
-
-        index = index + 1
-        pixel = pixels[x, y]
-
         try:
-            lcSet
-            lineComment = True
-        except NameError:
-            lineComment = False
+            column = column + 1
 
-        if debug:
-            print("Row " + str(row) + ", column " + str(column) + ", pixel " + str(pixel))
+            index = index + 1
+            pixel = pixels[x, y]
 
-        if not lineComment and pixel == (255, 255, 255):
+            try:
+                lcSet
+                lineComment = True
+            except NameError:
+                lineComment = False
+
             if debug:
-                print(str(index) + ": ASCII Mode OFF")
-            asciiMode = False
-        if not lineComment and pixel == (255, 0, 255):
-            if debug:
-                print(str(index) + ": Starting Line Comment")
-            lcSet = True
-            continue
-        if not lineComment and asciiMode and pixel != (255, 255, 255):
-            if debug:
-                print(str(index) + ": Processing ASCII")
+                print("Row " + str(row) + ", column " + str(column) + ", pixel " + str(pixel))
+
+            # General Operators
+            if not lineComment and pixel == (255, 255, 255):
+                if debug:
+                    print(str(index) + ": ASCII Mode OFF")
+                asciiMode = False
+            if not lineComment and pixel == (255, 0, 255):
+                if debug:
+                    print(str(index) + ": Starting Line Comment")
+                lcSet = True
                 continue
-            print(chr(pixel[0]), end="")
-        if not lineComment and pyMode and pixel != (255, 255, 255):
-            if debug:
-                print(str(index) + ": Processing Python code")
+            if not lineComment and asciiMode and pixel != (255, 255, 255):
+                if debug:
+                    print(str(index) + ": Processing ASCII")
+                print(chr(pixel[0]), end="")
+            if not lineComment and pixel == (255, 0, 0):
+                if debug:
+                    print(str(index) + ": Blank Pixel")
                 continue
-            pyCode = pyCode + chr(pixel[0])
-        if not lineComment and not pyMode and pixel == (0, 0, 0):
-            if debug:
-                print(str(index) + ": ASCII Mode ON")
-            asciiMode = True
-        if not lineComment and not asciiMode and pixel == (0, 255, 0):
-            if debug:
-                print(str(index) + ": Python Mode ON")
-            pyMode = True
-            pyCode = ""
-        if not lineComment and pixel == (0, 0, 255):
-            if debug:
-                print(str(index) + ": Python Mode OFF")
-            pyMode = False
-            exec(pyCode.rstrip("\0"))
+            if not lineComment and pyMode and pixel != (255, 255, 255):
+                if debug:
+                    print(str(index) + ": Processing Python code")
+                pyCode = pyCode + chr(pixel[0])
+            if not lineComment and not pyMode and pixel == (0, 0, 0):
+                if debug:
+                    print(str(index) + ": ASCII Mode ON")
+                asciiMode = True
+            if not lineComment and not asciiMode and pixel == (0, 255, 0):
+                if debug:
+                    print(str(index) + ": Python Mode ON")
+                pyMode = True
+                pyCode = ""
+            if not lineComment and pixel == (0, 0, 255):
+                if debug:
+                    print(str(index) + ": Python Mode OFF")
+                pyMode = False
+                exec(pyCode.rstrip("\0"))
 
-print("")
+            # Stack Management
+            if not lineComment and not pyMode and not asciiMode and pixel == (127, 0, 0):
+                if debug:
+                    print(str(index) + ": Dumping Stack")
+                print("STKD: Stack Dump:")
+                for i in programStack:
+                    print("    :" + str(i))
+                if len(programStack) < 1:
+                    print("    :<EMPT: Empty>")
+            if not lineComment and not pyMode and not asciiMode and pixel == (0, 0, 127):
+                if debug:
+                    print(str(index) + ": Flushing Stack")
+                programStack = []
+
+            # Stack Length Checker
+            if len(json.dumps(programStack)) > 2048: # Workaround here because for some reason using len(bytes(programStack)) just doesn't work
+                print("STKO: Stack Overflow")
+                quit()
+        except KeyboardInterrupt as err:
+            print("SEGV: Segmentation Fault")
+            logger.error(err)
+            print(err)
+            quit()
+
+if len(programStack) > 0:
+    print("MNEF: Memory Not Emptied Fault")
+    quit()
+
+print("SEC0: Successfully Exited")
